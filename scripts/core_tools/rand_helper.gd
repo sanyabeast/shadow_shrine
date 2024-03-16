c713dd83c533ee6f2da2c8d8a018b4faac172746
sanyabeast <a.gvrnsk@gmail.com>
2024-03-16 17:07:08 +0200

    project refactoring

---
+++ scripts/core_tools/rand_helper.gd
@@ -0,0 +1,33 @@
+# Author: @sanyabeast
+# Date: Feb. 2024
+
+class_name S2RandomnessManager
+const TAG: String = "RandomnessManager"
+
+var rng: RandomNumberGenerator = RandomNumberGenerator.new()
+var seed: int
+
+func set_seed(_seed: int):
+	seed = _seed
+	rng.seed = seed
+
+func random_bool() -> bool:
+	return rng.randi() % 2 == 0
+
+func range(from: float, to: float)->float:
+	return rng.randf_range(from, to)
+
+func choice_from_array(arr: Array) -> Variant:
+	var index: int =  rng.randi() % arr.size()
+	return arr[index]
+
+func choice_from_dict(dict: Dictionary) -> Variant:
+	var keys = dict.keys()
+	var index: int = rng.randi() % keys.size()
+	return dict[keys[index]]
+
+func randi()->int:
+	return rng.randi()
+	
+func randf()->int:
+	return rng.randf()
